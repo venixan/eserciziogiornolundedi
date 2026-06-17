dd1ecb25443703dea689ad2141a9c69dcb7bcd55
venixan <ve.bermudez@duocuc.cl>
2026-06-16 20:56:04 -0400

    se terminoooo T T

--- Ejerciciomenu.py
+++ Ejerciciomenu.py
@@ -88,4 +88,102 @@ def agregar_reserva(reservas):
 
         while True:
             cant=input("Ingrese cantidad de personas(2-20): ")
-            es_valido, cantidad = validar_cantidad_personas   
+            es_valido, cantidad = validar_cantidad_personas(cant)
+            if es_valido:
+                break
+            print("ERROR!! Cantidad inválida (debe ser un número entero entre 2 y 20).")
+
+        while True:
+            horas=input("Ingrese horas de reserva (1-8): ").strip()
+            es_valido, horas = validar_horas_reserva(horas)
+            if es_valido:
+                break
+            print("ERRO!! Horas inválidas (debe ser un número entero entre 1-8).")
+
+            reserva = {
+                "codigo_reserva": codigote,
+                "nombre_solicitante": nombre,
+                "tipo_sala": tipo,
+                "cantidad_personas": cantidad,
+                "horas_reserva": horas,
+                "confirmada": False
+            }      
+            reservas.append(reserva)
+            print("REserva registrada con éxito!!")
+
+def eliminar_reserva(reservas, codigo_reserva):
+    posicion = buscar_reserva(reservas, codigo_reserva)
+    if posicion == -1:
+        print("ERROR!! L reserva nofue encontrada.")
+    else:
+        reservas.pop(posicion)
+        print("Reserva eliminada con éxito!")
+
+def actualizar_confirmaciones(reservas):
+    for res in reservas:
+        if res["cantidad_personas"] >=10:
+            res["confirmada"] = True
+        else:
+            res["Confirmada"] = False
+    print("Estados de confirmación actualizados correctamente.")
+
+def mostrar_reservas(reservas):
+    if len(reservas) == 0:
+        print("No existen reservas almacenadas.")
+        return
+    print("---LISTA DE TOAS LAS RESERVAS---")
+    for res in reservas:
+        print("-" * 35)
+        reporte = {
+            "Código": res["codigo_reserva"],
+            "Solicitante": res["nombre_solicitante"],
+            "Tipo Sala": res["tipo_sala"],
+            "Personas": res["cantidad_personas"],
+            "Horas": res["horas_reserva"],
+            "Confirmada": "SÍ" if res["confirmada"] else "NO"
+        }                                 
+        for etiqueta, valor, in reporte.items():
+            print(f"- {etiqueta} : {valor}")
+        print("-" * 35) 
+        #estaba probando esto que lo vi en un video.
+
+def mostrar_reservas_confirmadas(reservas):
+    contador_confirmados = 0
+    print("----LISTA DE RESERVAS CONFIRMADAS----")
+    for res in reservas:
+        if res["confirmada"] == True:
+            contador_confirmados +=1
+            print("-" * 35)
+            print(f"Código: {res["codigo_reserva"]}")
+            print(f"Solicitante: {res["nombre_solicitante"]}")
+            print(f"Cantidad de personas: {res["cantidad_personas"]}")
+    if contador_confirmados == 0:
+        print("No hay registros de reservas confirmadas actualmente.")
+    else:
+        print("-"*35)
+
+lista_reservas =[] 
+while True:
+    mostrar_menu()
+    opcion = leer_opcion()
+    if opcion ==1:
+        agregar_reserva(lista_reservas)
+    elif opcion ==2:
+        codigo_buscar = input("Ingrese el código de reserva que desea buscar: ").strip()
+        posicion = buscar_reserva(lista_reservas, codigo_buscar)
+        if posicion == 1:
+            print("Reserva no encontrada.")
+        else:
+            print(f"Reserva encontrada en la posición: {posicion}")
+    elif opcion == 3:
+        codigo_eliminar=input("Ingrese el código de reserva que eliminara: ").strip()
+        eliminar_reserva(lista_reservas, codigo_eliminar)
+    elif opcion == 4:
+        actualizar_confirmaciones(lista_reservas)
+    elif opcion == 5:
+        mostrar_reservas(lista_reservas)
+    elif opcion == 6: 
+        print("Programa finalizado. Hasta luego!")
+        break
+    elif opcion == 7:
+        mostrar_reservas_confirmadas(lista_reservas)                            
